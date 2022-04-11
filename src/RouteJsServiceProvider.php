9c5c806b763b50587bb2c799dcdf15e9a75549e4
giorgosstab <tsaxre1991@hotmail.com>
2022-04-11 21:52:58 +0300

    Add pre system hook route-js provider

--- src/RouteJsServiceProvider.php
+++ src/RouteJsServiceProvider.php
@@ -1,13 +1,20 @@
 <?php
 
-namespace App\Providers;
+namespace Elegant\RouteJs;
 
-use Elegant\RouteJS\BladeRoute;
 use Elegant\Contracts\Hook\PostControllerConstructor;
+use Elegant\Contracts\Hook\PreSystem;
 use Elegant\Support\Facades\Blade;
 
-class RouteJsServiceProvider implements PostControllerConstructor
+class RouteJsServiceProvider implements PreSystem, PostControllerConstructor
 {
+    public function preSystem()
+    {
+        if (!file_exists(APPPATH . '/config/route-js.php')) {
+            copy(realpath(dirname(__DIR__) . '/../config/route-js.php'), APPPATH . '/config/route-js.php');
+        }
+    }
+
     public function postControllerConstructor(&$params)
     {
         Blade::directive('routes', function ($group) {
